eecc9ef1d889b79398d150c1459584b085584fd4
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2019-01-31 11:18:17 +0100

    :see_no_evil:

---
+++ config/webpack.server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+
+    mode: "development",
+
+    entry: {
+        yoquese: "./src/app.js"
+    },
+
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/"
+    },
+
+    devServer: {
+        contentBase: "dist"
+    }
+
+}
